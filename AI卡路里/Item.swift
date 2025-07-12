a771fb69c9fae34e4339baadc09d3ce09c294042
WeiProduct <1597498880weiproduct@gmail.com>
2025-07-12 17:19:24 -0400

    Initial Commit

---
+++ AI卡路里/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  AI卡路里
+//
+//  Created by weifu on 7/12/25.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
